be201b4ea5fb63d2d1d01d5c031e3041f0b1d71c
AlessioPopolo <alessio.popolo@si2001.it>
2021-06-30 12:33:40 +0200

    ADD add-auto-form.jsp, aggiornato RentalCarControllerServlet, AutomezzoDao e TipologiaAutomezzoDao per l'aggiunta di auto, e aggiunto bottone su lista-auto.jsp. testato e funzionante

--- src/main/java/com/rentalcar/dao/AutomezzoDao.java
+++ src/main/java/com/rentalcar/dao/AutomezzoDao.java
@@ -3,6 +3,7 @@ package com.rentalcar.dao;
 import com.rentalcar.entity.Automezzo;
 import com.rentalcar.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -10,6 +11,23 @@ public class AutomezzoDao {
     public List<Automezzo> getAllAutomezzi() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.createQuery("FROM Automezzo", Automezzo.class).list();
+        }
+    }
+
+    public void upsertAutomezzo(Automezzo automezzo) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // update the customer object
+            session.saveOrUpdate(automezzo);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/rentalcar/dao/TipologiaAutomezzoDao.java
+++ src/main/java/com/rentalcar/dao/TipologiaAutomezzoDao.java
@@ -1,8 +1,10 @@
 package com.rentalcar.dao;
 
 import com.rentalcar.entity.TipologiaAutomezzo;
+import com.rentalcar.entity.TipologiaUtente;
 import com.rentalcar.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -10,6 +12,13 @@ public class TipologiaAutomezzoDao {
     public List<TipologiaAutomezzo> getAllTipologie() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.createQuery("FROM TipologiaAutomezzo", TipologiaAutomezzo.class).list();
+        }
+    }
+
+    public TipologiaAutomezzo getCategoria(String categoria) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query query = session.createQuery("FROM TipologiaAutomezzo WHERE categoria = :miaCategoria ").setParameter("miaCategoria", categoria);
+            return (TipologiaAutomezzo) query.uniqueResult();
         }
     }
 }
